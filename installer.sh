666130d032e707117bc5e5cd5df0ed99ad70d746
Scott Cagno <scottiecagno@gmail.com>
2013-06-09 16:24:28 -0400

    removed old installer file

--- installer.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install --fix-missing bison libjack-dev python-sphinx python-pyside python-gst0.10-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libgstreamer0.10-cil-dev rygel-gst-launch gstreamer0.10-plugins-good gstreamer0.10-tools
