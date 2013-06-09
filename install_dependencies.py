613d1ce3ada7c35b3adb541eeaed640c51198215
Scott Cagno <scottiecagno@gmail.com>
2013-06-09 16:34:16 -0400

    added depen

--- install_dependencies.py
+++ install_dependencies.py
@@ -1,1 +1,1 @@
-sudo apt-get install --fix-missing bison libjack-dev python-sphinx python-pyside python-gst0.10-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libgstreamer0.10-cil-dev rygel-gst-launch gstreamer0.10-plugins-good gstreamer0.10-tools
+sudo apt-get install --fix-missing xdotool bison libjack-dev python-sphinx python-pyside python-gst0.10-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libgstreamer0.10-cil-dev rygel-gst-launch gstreamer0.10-plugins-good gstreamer0.10-tools
